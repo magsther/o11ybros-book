8872756d7ee4224ebcaf0a053b747de635e94e6f
Magsther <55060295+magsther@users.noreply.github.com>
2025-09-15 17:15:05 +0200

    Create app.py

---
+++ ch03-getting-started/app.py
@@ -0,0 +1,12 @@
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+span_processor = BatchSpanProcessor(OTLPSpanExporter())
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+with tracer.start_as_current_span("my-first-span"):
+    print("Hello from OpenTelemetry!")
